e85f6770fb30f42ae242043ed16541e3fcef080b
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-02-28 23:53:39 +0530

    sp

--- Day1/StarPattern2.java
+++ Day1/StarPattern2.java
@@ -1,10 +1,10 @@
 public class StarPattern2 {
     public static void main(String[] args) {
-        System.out.println("*****");
-        System.out.println("****");
-        System.out.println("***");
-        System.out.println("**");
         System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
     }
     
 }
