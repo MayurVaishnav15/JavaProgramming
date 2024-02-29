e04af2445ab67327894b3425daa1571a1d2a6740
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-01 00:15:12 +0530

    area of square

---
+++ Day1/AreaOfSQ.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AreaOfSQ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter side of square : ");
+        int side = sc.nextInt();
+        int Area = side * side;
+        System.out.println("Area of square is : ");
+        System.out.println(Area);
+        sc.close();
+    }
+}
+    
