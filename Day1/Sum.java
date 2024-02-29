1866241d9e00505f4b193ed31f89771d6158725c
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-02-29 10:47:03 +0530

    Added sum file

---
+++ Day1/Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 8;
+        float sum = a + b;
+        System.out.println(sum); //print sum of two numbers.
+    }
+}
