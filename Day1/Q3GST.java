8fd0bdad55919c4a9843a39de14c7add5619505e
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-01 00:31:16 +0530

    bill with GST

---
+++ Day1/Q3GST.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Q3GST {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter price of Pensil:");
+        float Pensil = sc.nextFloat();
+        System.out.println("Enter price of Pen: ");
+        float Pen =  sc.nextFloat();
+        System.out.println("Enter price of Eraser");
+        float Eraser = sc.nextFloat();
+        float gst = ((Pensil + Pen + Eraser)/100)*18;
+        float Total_Bill = (Pensil+Pen+Eraser)+gst;
+        System.out.println(Total_Bill);
+        sc.close();
+
+
+    }
+    
+}
