c78044b853a6e0c83368491139f9460cfa1e224c
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-01 21:39:28 +0530

    Odd and Even

--- Day3/OddEven.java
+++ Day3/OddEven.java
@@ -3,8 +3,8 @@ public class OddEven {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your number : ");
-        int a = sc.nextInt();
-        if(a%2 == 0){
+        int number = sc.nextInt();
+        if(number%2 == 0){
             System.out.println("The number is EVEN");
         }
         else{
