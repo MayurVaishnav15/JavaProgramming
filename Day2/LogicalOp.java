9f2d594cff8943695fdb690a5a52c7c0de57cb58
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-01 14:30:56 +0530

    Logical Operators

---
+++ Day2/LogicalOp.java
@@ -0,0 +1,10 @@
+public class LogicalOp {
+    public static void main(String[] args) {
+        int a = 10 ,b = 20;
+        System.out.println((a<b)&&('c' == 'c'));   //Give true if both statements are true.(&&)
+        System.out.println((a>b)||(9<11));     //Give ture if any one statement is true.(||)
+        System.out.println(a!=b);
+
+
+    }
+}
