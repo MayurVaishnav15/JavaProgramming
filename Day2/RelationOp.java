6d70022ef3123918f0f33d3a4224698186503722
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-01 14:16:44 +0530

    Relational Operators

---
+++ Day2/RelationOp.java
@@ -0,0 +1,12 @@
+public class RelationOp {
+    public static void main(String[] args) {
+        int A = 10;
+        // A = A + 40;             //This is slow .
+        A += 40;                   // This is more faster in java ....Saves Execution Time.
+        A -=  4;
+        A *= 10;
+        A /=  2;
+        System.out.println(A);
+    }
+    
+}
