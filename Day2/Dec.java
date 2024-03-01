ca75fc5a50ca3546a8b3a0b7ecf5baad77970719
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-01 13:38:36 +0530

    Decrement

---
+++ Day2/Dec.java
@@ -0,0 +1,13 @@
+public class Dec {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = --a;                   //First decrement the value and then assign it.
+        System.out.println(a);
+        System.out.println(b);
+
+        // int a = 20;
+        // int b = a--;                //First assign orignal value of a to b i.e 20 , and then decrement the value.
+        // System.out.println(a);
+        // System.out.println(b);
+    }
+}
