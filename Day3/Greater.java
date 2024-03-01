915a137c5c016e04814658007db65ca2f9239c86
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-01 21:32:07 +0530

    Greater of two numbers

--- Day3/Greater.java
+++ Day3/Greater.java
@@ -2,14 +2,15 @@ import java.util.*;
 public class Greater {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your two values : ");
+        System.out.print("Enter value1 : ");
     int a = sc.nextInt();
+    System.out.print("Enter value2 :");
     int b = sc.nextInt();
     if(a>b){
-        System.out.println("a is greater");
+        System.out.println("a is greater that is " + a);
     }
     else{
-        System.out.println("b is greater");
+        System.out.println("b is greater that is " + b);
     }
     sc.close();
     }
