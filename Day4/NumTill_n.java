78a9344a072a043bd88ee457651721f23f55c630
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-02 01:55:58 +0530

    Prints Numbers till n From User

---
+++ Day4/NumTill_n.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class NumTill_n {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = 0;
+        System.out.print("Enter number : ");
+        int a = sc.nextInt();
+        while(counter<=a){
+            System.out.print(counter + " ");
+            counter++;
+        }
+        sc.close();
+    }
+    
+}
