aebf5c398f9d79f7cbaf7619864a504abd67040a
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-02 10:48:09 +0530

    Reverse Printing

--- Day4/ReverseNo.java
+++ Day4/ReverseNo.java
@@ -2,12 +2,19 @@ public class ReverseNo {
     public static void main(String[] args) {
         int a = 10899;
         int reverse = 0;
-        for(int i =1;i<=5;i++){
-            int b;
-            b = a%10;
-            reverse = reverse * 10 + b;
-            a = a/10;
+        // for(int i =1;i<=5;i++){
+        //     int b;
+        //     b = a%10;
+        //     reverse = reverse * 10 + b;
+        //     a = a/10;
             
-        } System.out.println("The Reverse of a number is : " + reverse);
+        // } System.out.println("The Reverse of a number is : " + reverse);
+        while(a>0){
+            int LastDigit;
+            LastDigit = a%10;
+            reverse = reverse * 10 + LastDigit;
+            a = a/10;
+
+        }System.out.println(reverse);
     }
 }
