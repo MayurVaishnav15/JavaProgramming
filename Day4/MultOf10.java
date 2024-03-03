15ae5261ccc8af8d01be766e9adc9456cf39d2c1
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-03 22:49:18 +0530

    Break at multiple of 10

--- Day4/MultOf10.java
+++ Day4/MultOf10.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 public class MultOf10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for(int i = 1 ;true;i++){
+        for(int i = 1 ;true;){
             System.out.print("Enter your number : ");
             int a = sc.nextInt();
             if(a%10==0){
                 break;
             } System.out.println(a);
+            i=i+1;
         }System.out.println("Multiple of 10 found!!!....Loop Exited");
         sc.close();
     }
