720ffb8acbf40e2665b61633a198f1e008b93d53
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-03 19:22:56 +0530

    do_While Loop

---
+++ Day4/doWhile.java
@@ -0,0 +1,9 @@
+public class doWhile {
+    public static void main(String[] args) {
+        int counter = 1;
+        do{
+            System.out.println("helloWorld");
+            counter++;
+        }while(counter<=3);
+    }
+}
