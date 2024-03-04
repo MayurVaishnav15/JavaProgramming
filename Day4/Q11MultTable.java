877c675a61e0ce48b980a4d123e5bb1db43a676e
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-05 00:07:26 +0530

    Question 11 -> Multiply table of n

--- Day4/Q11MultTable.java
+++ Day4/Q11MultTable.java
@@ -1,15 +1,30 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Q11MultTable {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number for getting it's Multiplication Table : ");
-        int n = sc.nextInt();
-        int Table=0;
-        for(int i =1;i<=10;i++){
-            Table = n*i;
-            System.out.println(n+"*" + i + " = " +Table);
-        }sc.close();
-    }
+// public class Q11MultTable {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a number for getting it's Multiplication Table : ");
+//         int n = sc.nextInt();
+//         int Table=0;
+//         for(int i =1;i<=10;i++){
+//             Table = n*i;
+//             System.out.println(n+"*" + i + " = " +Table);
+//         }sc.close();
+//     }
     
+// }
+import java.util.Scanner;
+class Q11MultTable{
+    public static void main(String args[])
+    {
+        System.out.println("enter your number");
+        Scanner sc =new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(N+" x "+i+" = "+N*i);
+        }
+        sc.close();
+    }
 }
+
