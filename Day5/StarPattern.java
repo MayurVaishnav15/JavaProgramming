bd0dd3660918d12f7beff50de4a03065ffe20d0a
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-05 18:54:53 +0530

    half Pyramid StarPattern using Loops

--- Day5/StarPattern.java
+++ Day5/StarPattern.java
@@ -6,9 +6,9 @@ public class StarPattern {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your number : ");
         int n = sc.nextInt();
-        for(int i=1;i<=n;i++){                     // i is used for lines.
-            for(int j=1;j<=i;j++)
-            System.out.print("*");
+        for(int i=1;i<=n;i++){                     // i is used for lines;
+            for(int j=1;j<=i;j++)                  // j is used for stars;
+            System.out.print(" * ");
             System.out.print("\n");
         }
         sc.close();
