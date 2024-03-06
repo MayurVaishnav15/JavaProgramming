896b76acadf23515bb9eaccb87658927b3562789
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-07 01:56:41 +0530

    Introduction to funcitons

---
+++ Day6/Functions.java
@@ -0,0 +1,10 @@
+public class Functions {
+
+    public static void Helloworld() {
+        System.out.println("helloWorld");
+    }
+    public static void main(String[] args) {
+        Helloworld();
+    }
+    
+}
