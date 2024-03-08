d8c1b708e084ec290940169cee812d947fa000d3
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-08 23:35:41 +0530

    Factorial of a number using functions

---
+++ Day6/FactorialFunc.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FactorialFunc {
+
+    public static int factorialClass(int n){
+        int f=1;
+        for(int i = 1;i<=n;i++){
+            f*=i;
+        }
+        return f;
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter your value to calculate Factorial: ");
+        Scanner sc = new Scanner(System.in);
+        int F = sc.nextInt();
+        System.out.println(factorialClass(F));
+    }
+}
