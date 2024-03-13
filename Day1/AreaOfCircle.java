eaa424d25aa78c34eb1a74f470717eb302279e29
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-13 23:19:58 +0530

    normal changes

--- Day1/AreaOfCircle.java
+++ Day1/AreaOfCircle.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public class AreaOfCircle {
     public static void main(String[] args) {
-        System.out.println("Enter radius of a Circle : ");
+        System.out.print("Enter radius of a Circle : ");
         Scanner sc = new Scanner(System.in);
         float radius = (float)sc.nextFloat();
-        float pi = 3.14f;
+        float pi = 3.14f; //because default 3.14 will be taken as double and not float
         float AREA = pi * radius * radius;
-        System.out.println("The radius of circle is :  2");
+        System.out.print("The radius of circle is :  ");
         System.out.println(AREA);
         sc.close();
     }
