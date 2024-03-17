8522f827fd2e85fbd7bc58e653a459fdde1c18d9
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-17 23:37:01 +0530

    practicing the multiTable

---
+++ PRACTICE/multiTable.java
@@ -0,0 +1,17 @@
+package PRACTICE;
+
+import java.util.Scanner;
+
+public class multiTable {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter Your Number : ");
+    int number = sc.nextInt();
+    int table =1 ;
+    for(int i =1 ; i<=10 ; i++){
+        table=number*i;
+        System.out.println(number+"*"+i+"="+table);
+    }sc.close();
+    }
+
+}
