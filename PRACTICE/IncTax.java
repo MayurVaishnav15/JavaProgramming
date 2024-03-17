23becb3ddf42178be8cfd31ad6af00da00ceeb7e
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-17 23:50:08 +0530

    income tax calculator

---
+++ PRACTICE/IncTax.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class IncTax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Annual Income : ");
+        float income = sc.nextFloat();
+        if(income<500000){
+            System.out.println("You don't need to pay tax.");
+        }
+        else if(income>500000 && income<1000000){
+            System.out.println("You need to pay Tax of Rupees " + ((income/100)*20));
+        }
+        else{
+            System.out.println("You need to pay Tax of Rupees " + ((income/100)*30));
+        }
+        sc.close();
+    }
+}
