aa7477e9afab63d4ac28c03553a048c9f8a153f3
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-17 23:43:58 +0530

    reverse the number

---
+++ PRACTICE/reversing.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class reversing {
+    public static void main(String[] args) {
+        System.out.print("Enter Number you want to Reverse : ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int reverse = 0;
+        for(int i =1;a!=0;){
+            int b;
+            b = a%10;
+            reverse = reverse * 10 + b;
+            a = a/10;
+            i = i+1; //to overcome warning of compiler
+
+        } System.out.println("The Reverse of a number is : " + reverse);
+        sc.close();
+    }
+}
