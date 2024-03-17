b4c3da952ddd524b5565cf4438996d79c5b3c090
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-17 23:35:43 +0530

    factorial 2nd method i++

---
+++ Day4/factorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        System.out.println("Ready");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        // int factorial=1;
+        // for(int i=1;i<=number;i++){
+        //     factorial=factorial*i;
+        // }
+        // System.out.println(factorial);
+        int factorial=1;
+        for(int i=number;i>0;i--){
+            factorial*=i;
+        }
+        System.out.println(factorial);
+        sc.close();
+
+    }
+    
+}
