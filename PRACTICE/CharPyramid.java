4d87d2e2c2954efb802cf56ba650374008f0cb69
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-20 23:18:12 +0530

    practice

--- PRACTICE/CharPyramid.java
+++ PRACTICE/CharPyramid.java
@@ -8,7 +8,7 @@ public class CharPyramid {
         char ch ='A';
         for(int i=1;i<=number;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(" "+ ch+" ");
+            System.out.print(" "+ ch+" ");
                 ch++;
             }
             System.out.println("");
