5a4618ded7b6178308ed2ddade07b3d9414f83c3
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-20 23:11:18 +0530

    practice

--- PRACTICE/CharPyramid.java
+++ PRACTICE/CharPyramid.java
@@ -10,7 +10,7 @@ public class CharPyramid {
             for(int j=1;j<=i;j++){
                 System.out.print(" "+ ch+" ");
                 ch++;
-    }
+}
             System.out.println("");
             }
             sc.close();
