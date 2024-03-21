7dc1535428a36bd02dbb99c6f41102e022ebf335
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-03-21 23:55:00 +0530

    practice

--- PRACTICE/NumbTilln.java
+++ PRACTICE/NumbTilln.java
@@ -8,7 +8,7 @@ public class NumbTilln {
         while(counter<=a){
             System.out.print(counter + " ");
             counter++; 
-        }
+                }
         sc.close();
     }
 }
