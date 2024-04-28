1b1b6db139c544ef26f2777a3ec486ab5729876d
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-04-28 18:30:41 +0530

    type promotion

--- Day1/TypeProm.java
+++ Day1/TypeProm.java
@@ -1,9 +1,9 @@
 public class TypeProm {
     public static void main(String[] args) {
         byte a = 2;
-        // a = a*2;                    //this will be error due to Type Promotion in Java
-        a = (byte)(a*2);                       
-        System.out.println(a);        //can be resolved by type casting 
+        // a = a*2;                    //this will be error due to Type Promotion in Java because when we do operation, datatype of answer value changes to int autometically
+        a = (byte)(a*2);               //can be resolved by type casting 
+        System.out.println(a);       
     }
     
 }
