642486789f58e8da548468e8c63940b2cb8f1380
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-04-28 19:12:19 +0530

    continue statements in java

--- Day4/Continue.java
+++ Day4/Continue.java
@@ -2,14 +2,13 @@ import java.util.*;
 public class Continue{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for(int i = 1;true;){
+        while(true){       
             System.out.print("Enter your number : ");
             int num = sc.nextInt();
             if(num%10==0){
                 continue;
             }
-            System.out.println("Your number is "+num);
-            i = i+1;
+            System.out.println("Your number is "+num);         
             sc.close(); //remove this line before actually implementing the code : case of exception handling
         }
     } 
