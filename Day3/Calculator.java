8675f1983f42bb41856e96770f3db5b7129c6fd2
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-04-28 18:59:34 +0530

    calculator infinite loop

--- Day3/Calculator.java
+++ Day3/Calculator.java
@@ -2,19 +2,23 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter value1 :");
-        int num1 = sc.nextInt();
-        System.out.print("Enter value2 :");
-        int num2 = sc.nextInt();
-        System.out.print("Enter Operator : ");
-        char Operator = sc.next().charAt(0);
-        switch (Operator) {
-            case '+' :System.out.println(num1+num2);break;
-            case '-' :System.out.println(num1-num2);break;
-            case '*' :System.out.println(num1*num2);break;
-            case '/' :System.out.println(num1/num2);break;
-            case '%' :System.out.println(num1%num2);break;
-            default:System.out.println("Invalid Operator...");
-        }sc.close();
+        while (true) {
+            System.out.print("Enter value1 :");
+            int num1 = sc.nextInt();
+            System.out.print("Enter value2 :");
+            int num2 = sc.nextInt();
+            System.out.print("Enter Operator : ");
+            char Operator = sc.next().charAt(0);
+            switch (Operator) {
+                case '+' :System.out.println(num1+num2);break;
+                case '-' :System.out.println(num1-num2);break;
+                case '*' :System.out.println(num1*num2);break;
+                case '/' :System.out.println(num1/num2);break;
+                case '%' :System.out.println(num1%num2);break;
+                default:System.out.println("Invalid Operator...");
+                sc.close();
+             } 
+        }
+       
     }
 }
