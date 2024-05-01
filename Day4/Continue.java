155c1e165b8f7f8aba7c4bf5d79cc81c6bf6af27
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-05-01 06:47:58 +0530

    edited Continue.java

--- Day4/Continue.java
+++ Day4/Continue.java
@@ -6,10 +6,10 @@ public class Continue{
             System.out.print("Enter your number : ");
             int num = sc.nextInt();
             if(num%10==0){
-                continue;
+                continue; //loop exits and restart if number is multiple of 10
             }
             System.out.println("Your number is "+num);         
-            sc.close(); //remove this line before actually implementing the code : case of exception handling
+            // sc.close(); //remove this line before actually implementing the code : case of exception handling
         }
     } 
 }
