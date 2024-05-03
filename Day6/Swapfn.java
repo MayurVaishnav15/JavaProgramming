b8e8080a861727e1d6a759e109600bfd249b7274
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-05-03 11:36:41 +0530

    swaping values with function

---
+++ Day6/Swapfn.java
@@ -0,0 +1,29 @@
+package Day6;
+import java.util.*;
+public class Swapfn {
+    public static void swapFunction(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+    System.out.println();
+    System.out.println("Swapped Values are :-");
+    System.out.println("A = "+a);
+    System.out.println("B = "+b);
+
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Numbers for swaping");
+        System.out.print("A = ");
+        int a = sc.nextInt();
+        System.out.print("B = ");
+        int b = sc.nextInt();
+        swapFunction(a,b);
+
+
+
+        sc.close();
+
+    }
+}
