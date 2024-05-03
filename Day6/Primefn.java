b1da7c677337eaa502765252ec6a27073c62377b
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-05-03 19:22:14 +0530

    prime or not using fn

---
+++ Day6/Primefn.java
@@ -0,0 +1,29 @@
+package Day6;
+import java.util.*;
+
+public class Primefn {
+
+    public static boolean Prime(int n ){
+        boolean isPrime = true;
+        for(int i = 2 ; i<=n-1;i++){
+            if(n%i == 0 ){
+                return false;          
+            }
+        }return isPrime;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number : ");
+        int n = sc.nextInt();
+        boolean result = Prime(n);
+        if(result == false){
+            System.out.println("Not Prime");
+        }
+        else{
+            System.out.println("Prime");
+        }
+        sc.close();
+
+      
+    }
+}
