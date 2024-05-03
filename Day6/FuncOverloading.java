31f76d03a92451e78ebaa689d9ca1802f53ae10a
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-05-03 18:32:05 +0530

    func overing by sum fn

---
+++ Day6/FuncOverloading.java
@@ -0,0 +1,27 @@
+package Day6;
+import java.util.*;
+
+public class FuncOverloading {
+    public static int sum(int a , int b){
+        return a+b;
+        
+    }
+    public static int sum(int a , int b, int c){
+        return a+b+c;
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First value : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second value : ");
+        int b = sc.nextInt();
+        System.out.print("Enter Third value : ");
+        int c = sc.nextInt();
+        System.out.println(sum(a,b));
+        System.out.println(sum(a,b,c));
+        sc.close();
+        
+    }
+    
+}
