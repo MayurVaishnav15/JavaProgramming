bb02f00c2f5700934fe491f79dab41ed06d1bf12
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-05-04 22:22:14 +0530

    bin to dec

--- Day6/BinaryToDecimal.java
+++ Day6/BinaryToDecimal.java
@@ -1,7 +1,24 @@
 import java.util.*;
 public class BinaryToDecimal {
+    public static void Operations(int binNum){
+    int myName = binNum;
+     int pow = 0;
+     int decNum = 0;
+     while(binNum > 0){
+        int lastDigit = binNum%10;
+        decNum = decNum + (int)(lastDigit* Math.pow(2,pow));
+        pow++;
+        binNum = binNum/10;
+     }
+     System.out.println("The Decimal of " +myName + " is " + decNum);
+
+    }
     public static void main(String[] args) {
-        System.out.println("ready");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Binary Number : ");
+        int n = sc.nextInt();
+        Operations(n);
+        sc.close();
     }
     
 }
