c7473e5a0e369194de83c299dcc5e9d68db16e02
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-05-05 09:59:28 +0530

    odd even by functions

---
+++ Day6/oddEvenFn.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class oddEvenFn {
+    public static boolean isEven(int number) {
+
+        if (number % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int a = sc.nextInt();
+        // sc.close();
+        if (isEven(a)) {
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+    }
+
+}
