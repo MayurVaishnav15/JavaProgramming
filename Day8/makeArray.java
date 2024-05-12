a92d3ff6b62ef7993ce6ce191649402a62527581
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-05-12 23:59:29 +0530

    making Array

---
+++ Day8/makeArray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class makeArray {
+    public static void makingArray(){
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Array[] = new int[10];
+        for(int i = 0; i<Array.length;i++){
+            Array[i] = sc.nextInt();
+            System.out.print(Array[i]+ " ");
+        }
+        sc.close();
+        
+    }
+}
