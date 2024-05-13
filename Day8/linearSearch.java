e8e323efb556497e661e33f62f043c534b74a1a8
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-05-14 00:55:33 +0530

    linear Search

--- Day8/linearSearch.java
+++ Day8/linearSearch.java
@@ -10,6 +10,7 @@ public class linearSearch {
         return -1;
     }
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int numbers[] = new int[5];
         System.out.println("Enter Array Elements :-");
