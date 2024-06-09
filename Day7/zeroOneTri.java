18c85b3d54f41c9f7bccb27d52702234a29602f9
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-06-09 22:54:57 +0530

    zero triangle

--- Day7/zeroOneTri.java
+++ Day7/zeroOneTri.java
@@ -19,7 +19,8 @@ public class zeroOneTri {
         System.out.print("Enter your number : ");
         int n = sc.nextInt();
         zero_One_Triangle(n);
-        sc.close();  
+        sc.close();
+          
     }
     
 }
