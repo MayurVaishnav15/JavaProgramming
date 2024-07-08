b09531fe1c4dd414ac79f216a21133de073202d2
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-07-08 12:26:11 +0530

    Function Overloading

--- Day6/FuncOverloading.java
+++ Day6/FuncOverloading.java
@@ -1,13 +1,10 @@
 import java.util.*;
-
 public class FuncOverloading {
     public static int sum(int a , int b){
         return a+b;
-        
     }
     public static int sum(int a , int b, int c){
         return a+b+c;
-        
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -17,10 +14,8 @@ public class FuncOverloading {
         int b = sc.nextInt();
         System.out.print("Enter Third value : ");
         int c = sc.nextInt();
-        System.out.println(sum(a,b));
-        System.out.println(sum(a,b,c));
-        sc.close();
-        
-    }
-    
+        System.out.println("The sum of first two number is  : " +sum(a,b));
+        System.out.println("The sum of all numbers is :"+ sum(a,b,c));
+        sc.close();  
+    }   
 }
