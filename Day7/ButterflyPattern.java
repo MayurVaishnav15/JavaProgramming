aee6e9067eec2e9c5960a3e87bf64d1b1798cf71
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-07-18 14:15:12 +0530

    Butterfly Pattern wide

--- Day7/ButterflyPattern.java
+++ Day7/ButterflyPattern.java
@@ -3,26 +3,29 @@ import java.util.Scanner;
 public class ButterflyPattern {
     public static void ButterFlyPattern(int n){
         for(int i=1;i<=n;i++){
+            //stars- i
             for(int j=1;j<=i;j++){
-                System.out.print("*");
+                System.out.print(" * ");
             }
-            for(int j=1;j<=2*(n-1);j++){
-                System.out.print(" ");
+            //Spaces- 2*(n-i)
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print("   ");
             }
+            //stars- i
             for(int j=1;j<=i;j++){
-                System.out.print("*");
+                System.out.print(" * ");
             }
             System.out.println();
         }
-        for(int i=1;i>=n;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){ 
+                System.out.print(" * ");
             }
-            for(int j=1;j<=2*(n-1);j++){
-                System.out.print(" ");
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print("   ");
             }
             for(int j=1;j<=i;j++){
-                System.out.print("*");
+                System.out.print(" * ");
             }
             System.out.println();
         }
