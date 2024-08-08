d330045ffc25d5c8a9fea9f7dc4c77306fa562e0
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-08-08 08:39:13 +0530

    for nptel solving

---
+++ PRACTICE/nptel.java
@@ -0,0 +1,9 @@
+public class nptel{
+    public static void main(String[] args) {
+        int sum =0;int n = 0;
+        for(n=0;n<=7;n++){
+            sum+=n;
+        }System.out.println(sum);
+       
+    }
+}
