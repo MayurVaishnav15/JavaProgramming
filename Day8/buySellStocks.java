e4f8cdf09468051665f5fab94e6248d15d107c27
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-08-09 18:43:11 +0530

    about stocks

---
+++ Day8/buySellStocks.java
@@ -0,0 +1,12 @@
+public class buySellStocks {
+    public static void Stocks(int prices[]){
+        // int buyPrice = Integer.MAX_VALUE;
+       
+    }
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        Stocks(prices);
+
+    }
+    
+}
