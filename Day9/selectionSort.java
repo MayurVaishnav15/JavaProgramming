32a99aa60b0308d97a0a78a3471b2ffab748006c
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-08-11 23:45:15 +0530

    Selection Sort

--- Day9/selectionSort.java
+++ Day9/selectionSort.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
 
 public class selectionSort {
+    // We select an element from Array and push to the starting of Array - Selection Sort
+    public static void Selectionsort(int unsorted[]){
+        for(int i=0;i<unsorted.length;i++){
+            int minPos=i;
+            for(int j=minPos+1;j<unsorted.length;j++){
+                if(unsorted[minPos]>unsorted[j]){
+                    minPos=j;
+                }
+            }
+            int temp = unsorted[minPos];
+            unsorted[minPos]=unsorted[i];
+            unsorted[i]=temp;
+            }System.out.print("The Sorted Array is :-  ");
+            for(int i=0;i<unsorted.length;i++){
+                System.out.print(unsorted[i]+", ");
+            }
+        }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your Array size:- ");
@@ -11,8 +28,10 @@ public class selectionSort {
             unsorted[i]=sc.nextInt();
         }System.out.print("The Unsorted array is :-  ");
         for(int i=0;i<unsorted.length;i++){
-            System.out.print(unsorted[i]+", ");
-        }sc.close();
+            System.out.print(unsorted[i]+", ");   
+        }System.out.println();
+        sc.close();
+        Selectionsort(unsorted);
         
 
     }
