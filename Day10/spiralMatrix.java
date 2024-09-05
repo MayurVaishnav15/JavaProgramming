58cbd8b0d1d2484e22ae020c02556c28992f3368
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2024-09-06 00:43:36 +0530

    printing sprial matrix

--- Day10/spiralMatrix.java
+++ Day10/spiralMatrix.java
@@ -6,7 +6,35 @@ public class spiralMatrix {
         int startCol =0;
         int endRow = matrix.length-1;
         int endCol = matrix[0].length-1;
-         
+        while(startRow<=endRow && startCol<=endCol){
+            //top boundary
+            for(int j=startCol;j<=endCol;j++){
+                System.out.print(matrix[startRow][j]+" ");
+            }
+            //right boundary
+            for(int i=startRow+1;i<=endRow;i++){
+                System.out.print(matrix[i][endCol]+" ");
+            }
+            //bottom boundary
+            for(int j=endCol-1;j>=startCol;j--){
+                if(startRow==endRow){
+                    break;
+                }
+                System.out.print(matrix[endRow][j]+" ");
+            }
+            //left boundary
+            for(int i=endRow-1;i>=startRow+1;i--){
+                if(startCol==endCol){
+                    break;
+                }
+                System.out.print(matrix[i][startCol]+" ");
+            }
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
+        }
+        System.out.println();         
 
     }
     public static void main(String[] args) {
