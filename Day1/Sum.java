090bfd631baa57a366d4d74d69d21992deb14988
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-07-07 10:28:59 +0530

    practice

--- Day1/Sum.java
+++ Day1/Sum.java
@@ -2,7 +2,8 @@ public class Sum {
     public static void main(String[] args) {
         int a = 5;
         int b = 8;
-        float sum = a + b;
+        int sum;
+        sum = a + b;
         System.out.println(sum); //print sum of two numbers.
     }
 }
